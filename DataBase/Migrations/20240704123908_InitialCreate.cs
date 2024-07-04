31f05ae3d2044892c3de77202a6d9eeb807ce055
KostPost <kt5082206@gmail.com>
2024-07-04 17:14:44 +0200

    first

---
+++ DataBase/Migrations/20240704123908_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_account",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    steam_id = table.Column<string>(type: "text", nullable: false),
+                    user_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_account", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_account");
+        }
+    }
+}
---
+++ ModelClasses/UserAccount.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelClasses;
+
+[Table("user_account")]
+public class UserAccount
+{
+    [Key] [Column("id")] public long Id { get; set; }
+    [Column("user_name")] public string UserName { get; set; }
+    [Column("steam_id")] public string SteamId { get; set; }
+    [Column("current_mmr")] public int CurrentMMR { get; set; }
+    [Column("goal_mmr")] public int GoalMMR { get; set; }
+    [NotMapped] public int RegularMatches { get; set; }
+    [NotMapped] public int DoubleMatches { get; set; }
+}
